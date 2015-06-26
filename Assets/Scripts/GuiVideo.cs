07dced590d6f853ed858f967cb4344a002aa3eb0
Reid Blomquist <reid@theprogrampdx.com>
2015-06-25 23:32:00 -0700

    minor scene/audio tweaks; cleaning up GuiVideo

--- Assets/Scripts/GuiVideo.cs
+++ Assets/Scripts/GuiVideo.cs
@@ -4,19 +4,11 @@ using System.Collections;
 
 public class GuiVideo : MonoBehaviour {
 
-	private AudioSource sound;
-
 	void Start() {
 		MovieTexture movie = GetComponent<RawImage>().texture as MovieTexture;
-		sound = GetComponent<AudioSource>();
+		AudioSource sound = GetComponent<AudioSource>();
 		sound.clip = movie.audioClip;
 		movie.Play();
 		sound.Play();
 	}
-	
-//	void Update() {
-//		if (!sound.isPlaying) {
-//			SceneLoader.NextScene();
-//		}
-//	}
 }
