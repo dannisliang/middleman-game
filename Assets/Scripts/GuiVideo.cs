9869a245894dbb085afd33582a11eac146062626
Reid Blomquist <reid@theprogrampdx.com>
2015-06-25 23:14:20 -0700

    UI Raw Image videos now easily addable; ignoring video assets in repo (TODO figure out how to pull this in outside of git)

---
+++ Assets/Scripts/GuiVideo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GuiVideo : MonoBehaviour {
+
+	private AudioSource sound;
+
+	void Start() {
+		MovieTexture movie = GetComponent<RawImage>().texture as MovieTexture;
+		sound = GetComponent<AudioSource>();
+		sound.clip = movie.audioClip;
+		movie.Play();
+		sound.Play();
+	}
+	
+//	void Update() {
+//		if (!sound.isPlaying) {
+//			SceneLoader.NextScene();
+//		}
+//	}
+}
